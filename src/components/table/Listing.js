2dc40943d13a0c8ca80ae2073bfd2779f12061b2
80220 <43602145+80220@users.noreply.github.com>
2021-09-21 09:28:11 +0200

    svg change

--- src/components/table/Listing.js
+++ src/components/table/Listing.js
@@ -5,7 +5,7 @@ import "./listing.css";
 
 const captitionStyle = css`
   background: #ddd;
-  padding: 10px;
+  padding: 5px;
   font-family: "Tahoma";
   font-weight: 700;
   font-size: 0.9em;
@@ -72,14 +72,14 @@ const inputFilterCSS = css`
   }
 `;
 
-const filterbuttonCSS = css`
-  height: 14px;
-  width: 7px;
-  /* border: 1px solid black; */
-  /* border-radius: 10px;   */
-  box-sizing: content-box;
+const filterIconContainerCSS = css`
+  display: inline-block;
+  width: 15px;
+  height: 15px;
+  max-width: 15px;
+  max-height: 15px;
   position: relative;
-  left: -30px;
+  top: 3px;
 `;
 
 const checkboxHeaderCSS = css`
@@ -92,6 +92,7 @@ const removeButttonCSS = css`
   background: red;
   color: white;
 `;
+
 function Listing({ items, name, sort }) {
   const [content, setContent] = useState([]);
   const [currentCol, setCurrentCol] = useState(-1);
@@ -155,9 +156,48 @@ function Listing({ items, name, sort }) {
 
   return (
     <>
+      {/* icons */}
+      <svg style={{ display: "none" }} version="2.0">
+        <defs>
+          <symbol
+            id="filter-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-filter"
+          >
+            <polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3" />
+          </symbol>
+          <symbol
+            id="cancel-icon"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            viewBox="0 0 72.434 72.44"
+          >
+            <path
+              d="M36.22,0C16.212,0,0,16.216,0,36.227c0,19.999,16.212,36.214,36.22,36.214
+c20.011,0,36.214-16.215,36.214-36.214C72.434,16.215,56.231,0,36.22,0z M52.058,46.82c1.379,1.424,0.953,4.078-0.959,5.932
+c-1.911,1.854-4.577,2.209-5.959,0.785l-9.027-9.295l-9.298,9.027c-1.421,1.379-4.075,0.947-5.929-0.961s-2.206-4.574-0.785-5.956
+l9.298-9.027l-9.027-9.298c-1.379-1.421-0.946-4.078,0.962-5.932c1.905-1.851,4.577-2.204,5.953-0.785l9.027,9.297l9.301-9.023
+c1.424-1.382,4.078-0.95,5.929,0.958c1.857,1.908,2.206,4.577,0.785,5.959l-9.295,9.024L52.058,46.82z"
+            />
+          </symbol>
+        </defs>
+      </svg>
       {/*** filtering ***/}
       {filteredColumn ? (
         <label css={labelFilterCSS}>
+          <div css={filterIconContainerCSS}>
+            <svg viewBox="0 0 24 24">
+              <use href="#filter-icon" />
+            </svg>
+          </div>
           {filteredColumn}
           {": "}
           <input
@@ -207,21 +247,8 @@ function Listing({ items, name, sort }) {
               );
             }}
           >
-            <svg
-              version="1.1"
-              id="Layer_1"
-              xmlns="http://www.w3.org/2000/svg"
-              x="0px"
-              y="0px"
-              viewBox="0 0 72.434 72.44"
-            >
-              <path
-                d="M36.22,0C16.212,0,0,16.216,0,36.227c0,19.999,16.212,36.214,36.22,36.214
-	c20.011,0,36.214-16.215,36.214-36.214C72.434,16.215,56.231,0,36.22,0z M52.058,46.82c1.379,1.424,0.953,4.078-0.959,5.932
-	c-1.911,1.854-4.577,2.209-5.959,0.785l-9.027-9.295l-9.298,9.027c-1.421,1.379-4.075,0.947-5.929-0.961s-2.206-4.574-0.785-5.956
-	l9.298-9.027l-9.027-9.298c-1.379-1.421-0.946-4.078,0.962-5.932c1.905-1.851,4.577-2.204,5.953-0.785l9.027,9.297l9.301-9.023
-	c1.424-1.382,4.078-0.95,5.929,0.958c1.857,1.908,2.206,4.577,0.785,5.959l-9.295,9.024L52.058,46.82z"
-              />
+            <svg viewBox="0 0 72.434 72.44">
+              <use href="#cancel-icon" />
             </svg>
           </div>
         </label>
@@ -276,23 +303,24 @@ function Listing({ items, name, sort }) {
                         </span>
                         <span className="sortable arrow-down"></span>
                       </div>
-                      <svg
-                        onClick={(e) => {
-                          setFilteredColumn(key);
+                      <div
+                        style={{
+                          display: "inline-block",
+                          width: "22px",
+                          height: "20px",
+                          maxWidth: "22px",
+                          maxHeight: "20px"
                         }}
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        width="14"
-                        height="14"
-                        fill="blue"
-                        stroke="blue"
-                        strokeWidth="0.1"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        className="feather feather-filter"
                       >
-                        <polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"></polygon>
-                      </svg>
+                        <svg
+                          viewBox="0 0 24 24"
+                          onClick={(e) => {
+                            setFilteredColumn(key);
+                          }}
+                        >
+                          <use href="#filter-icon" />
+                        </svg>
+                      </div>
                     </div>
                   </th>
                 );
